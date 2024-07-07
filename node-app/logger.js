64bb1eb88b9ea5dcdcf439067ba4bbaba8beccb6
“Himanshu <hranjan120@gmail.com>
2024-07-07 19:28:17 +0530

    logger format updated

--- node-app/logger.js
+++ node-app/logger.js
@@ -1,18 +1,44 @@
-const winston = require("winston");
+const { createLogger, transports, format } = require('winston');
 const LogstashTransport = require("winston-logstash/lib/winston-logstash-latest");
 
-const loggerMethod = () => {
-    const logger = winston.createLogger({
+/*-------------------*/
+const loggerMethod = (logType, msg, appName) => {
+    const logger = createLogger({
+        format: format.combine(
+            format.label({ label: appName }),
+            format.timestamp(),
+            format.errors({ stack: true }), // Include stack traces (if any)
+            format.splat(), // Allows logging objects as well
+            format.json()
+        ),
+        defaultMeta: { myCustomField: 'my custom field value', environment: 'dev' },
         transports: [
             new LogstashTransport({
                 port: 28777,
                 node_name: "my-logger-node",
-                host: "http://logstash:5044",
+                host: "logstash",
             }),
         ],
     });
 
-    logger.info("Hello world!");
+    if (logType === 'info') {
+        logger.info(msg);
+    }
+    if (logType === 'error') {
+        logger.error(msg);
+    }
+    if (logType === 'warn') {
+        logger.warn(msg);
+    }
+    if (logType === 'verbose') {
+        logger.verbose(msg);
+    }
+    if (logType === 'debug') {
+        logger.debug(msg);
+    }
+    if (logType === 'silly') {
+        logger.silly(msg);
+    }
 
     return { status: true, msg: 'success' };
 }
--- node-app/server.js
+++ node-app/server.js
@@ -8,15 +8,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/info-log', (req, res) => {
-    loggerMethod();
+    loggerMethod('info', 'Any number of formats may be combined into a single format using format.combine.', 'NODE-APP-1');
     res.send('Info Log Added to Logstash');
 })
 
 app.get('/error-log', (req, res) => {
+    loggerMethod('error', 'Error number of formats may be combined into a single format using format.combine.', 'NODE-APP-1');
     res.send('Error Log Added to Logstash')
 })
 
 app.get('/warning-log', (req, res) => {
+    loggerMethod('warn', 'Warn of formats may be combined into a single format using format.combine.', 'NODE-APP-2');
     res.send('Warning Log Added to Logstash')
 })
 
